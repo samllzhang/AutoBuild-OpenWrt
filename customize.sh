6ce0224a5a291bc329d8106cd3edfd91fb24ea4c
samllzhang <68906880+samllzhang@users.noreply.github.com>
2022-04-12 20:38:49 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,7 +5,7 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
+#1. 修改默认IP
 sed -i 's/192.168.1.1/192.168.1.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
